45617b98b55b05ce48382c5098ea16d8e01598fb
Roop444 <rupinderk@iiitd.ac.in>
2026-04-20 23:01:23 +0530

    Update tamper_proxy.c

--- tamper_proxy.c
+++ tamper_proxy.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
+
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define LISTEN_PORT 6666
 #define SERVER_PORT 5555
@@ -13,6 +16,8 @@ int main()
     int lfd = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in a;
+    memset(&a, 0, sizeof(a));
+
     a.sin_family = AF_INET;
     a.sin_port = htons(LISTEN_PORT);
     a.sin_addr.s_addr = INADDR_ANY;
@@ -23,16 +28,20 @@ int main()
     printf("Tamper proxy listening on %d\n", LISTEN_PORT);
 
     int cfd = accept(lfd, NULL, NULL);
+
     printf("Client connected.\n");
 
     int sfd = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in srv;
+    memset(&srv, 0, sizeof(srv));
+
     srv.sin_family = AF_INET;
     srv.sin_port = htons(SERVER_PORT);
     inet_pton(AF_INET, "127.0.0.1", &srv.sin_addr);
 
     connect(sfd, (struct sockaddr *)&srv, sizeof(srv));
+
     printf("Connected to real server.\n");
 
     unsigned char buf[4096];
@@ -42,7 +51,7 @@ int main()
     while ((n = read(cfd, buf, sizeof(buf))) > 0) {
 
         if (!tampered && n > 40) {
-            buf[30] ^= 0xFF;   /* flip one byte */
+            buf[30] ^= 0xFF;
             tampered = 1;
             printf("Ciphertext tampered!\n");
         }
@@ -53,5 +62,6 @@ int main()
     close(cfd);
     close(sfd);
     close(lfd);
+
     return 0;
 }
